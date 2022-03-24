d1862eb9094cd50fdcc91e1f7de7c3e0e7d57491
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-03-25 00:00:47 +0100

    kolopay constant styles added

---
+++ constants/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// styled-components
+import { } from "../styles/constants/Constants";
+
+const Button = () => {
+    return (
+        <>
+            <h1>Button</h1>
+        </>
+    )
+}
+
+export default Button
